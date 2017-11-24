138535daeb63b3b51fec81a2c3c562ec1ab54e8c
David Fernandez <davferrod3@alum.us.es>
2017-11-24 00:19:20 +0000

    First version with Auth0

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var db = require('./db');
 var path = require('path');
+
 global.__root   = __dirname + '/'; 
 
 app.get('/api', function (req, res) {
@@ -16,6 +17,9 @@ app.use('/api/auth', AuthController);
 
 var ResearcherController = require(__root + 'researcher/ResearcherController');
 app.use('/api/v1', ResearcherController);
+
+var ResearcherSecureController = require(__root + 'researcher/ResearcherSecureController');
+app.use('/api/v1.1', ResearcherSecureController);
 
 
 app.use(express.static(path.join(__dirname, "public")));
---
+++ researcher/ResearcherSecureController.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
+var jwtAuthz = require('express-jwt-authz');
+var researcherModel = require('./Researcher');
+
+/* Create middleware for checking the JWT */
+var checkScopes = jwtAuthz(["read:messages"]);
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: "https://si1718-dfr-researchers.eu.auth0.com/.well-known/jwks.json"
+    }),
+    audience: 'https://si1718-dfr-researchers.com',
+    issuer: "https://si1718-dfr-researchers.eu.auth0.com/",
+    algorithms: ['RS256']
+});
+
+/* configura dentro de express el middleware bodyparser json */
+router.use(bodyParser.json());
+
+/* Método GET que devuelve todos los investigadores */
+router.get('/researchers', jwtCheck, function (request, response) {
+    
+    var param = request.query.search;
+    var query = {};
+    if (param){
+        query = { $or:[ 
+            {'name':{ $regex: '.*' + param + '.*', $options: 'i' }}, 
+            {'phone':{ $regex: '.*' + param + '.*', $options: 'i' }}, 
+            {'orcid':{ $regex: '.*' + param + '.*', $options: 'i' }},
+            {'researcherId':{ $regex: '.*' + param + '.*', $options: 'i' }}, 
+            {'link':{ $regex: '.*' + param + '.*', $options: 'i' }}, 
+            {'idGroup':{ $regex: '.*' + param + '.*', $options: 'i' }}, 
+            {'idDepartment':{ $regex: '.*' + param + '.*', $options: 'i' }}, 
+            {'professionalSituation':{ $regex: '.*' + param + '.*', $options: 'i' }} 
+        ]};
+    }
+    
+    console.log("INFO: New GET request to /researchers");
+    researcherModel.find(query, function (err, researchers) {
+        
+        if (err) {
+            console.error('WARNING: Error getting data from DB');
+            response.sendStatus(500); // internal server error
+        } else {
+            console.log("INFO: Sending researchers: " + JSON.stringify(researchers, 2, null));
+            response.send(researchers);
+        }
+    });
+});
+
+module.exports = router;
